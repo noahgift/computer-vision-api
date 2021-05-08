17606e9059c0f6708ea52074f9961502ffc86ad6
Noah Gift <noah.gift@gmail.com>
2021-05-08 13:39:11 +0000

    adding working computer vision tool

--- hello.py
+++ hello.py
@@ -1,14 +1,28 @@
+#!/usr/bin/env python3
+
+import click
 import boto3
 
-client = boto3.client('rekognition')
-
-response = client.detect_labels(
-    Image={
-        'S3Object': {
-            'Bucket': 'aws-ml-guide',
-            'Name': 'Marvel.jpg',
+@click.command(help="This tool does label detection")
+@click.option('--file', prompt='I need the name of the file in the bucket',
+              help='This is the name of the file in the bucket')
+def detect(file):
+    client = boto3.client('rekognition')
+    response = client.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': 'aws-ml-guide',
+                'Name': file,
+            },
         },
-    },
-)
+    )
+    click.echo(click.style(f"Detecting Labels for: {file}", fg="red"))
+    labels = response['Labels']
+    for label in labels:
+        name = label['Name']
+        confidence = label['Confidence']
+        click.echo(click.style(f"{name}: {confidence}", fg="green"))
 
-print(response)
+if __name__ == '__main__':
+    # pylint: disable=E1120
+    detect()
